4812ccea12c053e9001900365a00588e41f637f5
aviashok96 <avinashkumarakj@gmail.com>
2017-02-06 20:32:27 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 app.get('/article-one', function(re2, res){
-    res.send('avi...how are you?');
+     res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 });
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
